04cb44962b0a3e3aed83f1c04ea124c8dfb39ba3
MaciolekMichal <167910086+MaciolekMichal@users.noreply.github.com>
2024-08-24 13:40:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,18 +4,18 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "terraform_bucket" {
-  name = "terraform-bucket-yo"
+  name = "gitops-terraform-bucket-${var.branch_name}"
   location = "EU"
 }
 
 resource "google_bigquery_dataset" "terraform_dataset" {
-  dataset_id = "terraform_dataset"
+  dataset_id = "terraform_dataset-${var.branch_name}"
   location = "EU"
 }
 
 terraform {
   backend "gcs" {
     bucket = "bucket-for-terraform-states"
-    prefix = "env/var.branch_name"
+    prefix = "env/${var.branch_name}"
   }
 }
